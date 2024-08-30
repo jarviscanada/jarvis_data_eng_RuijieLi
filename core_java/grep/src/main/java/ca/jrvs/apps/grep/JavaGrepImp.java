3889c50e500773a2a176c0a3d4ea3f6fce77a3fe
Rui Jie Li <rui-jie.li@polymtl.ca>
2024-08-30 19:17:58 -0400

    grep: done

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -1,14 +1,18 @@
 package ca.jrvs.apps.grep;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Stream;
 import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
@@ -41,7 +45,19 @@ public class JavaGrepImp implements JavaGrep {
 
     @Override
     public void process() throws IOException {
-//        this.listFiles(this.rootPath);
+        List<File> files = listFiles(rootPath);
+        List<String> matchedLines = new ArrayList<>();
+        for(File file:files) {
+            List<String> lines = readLines(file);
+            for(String line:lines) {
+                if(containsPattern(line)) {
+                    matchedLines.add(
+                        file.getPath() + " : " + line
+                    );
+                }
+            }
+        }
+        writeToFile(matchedLines);
     }
 
     @Override
@@ -56,7 +72,7 @@ public class JavaGrepImp implements JavaGrep {
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("IO Exception", e);
         }
         this.logger.debug(files.toString());
         return files;
@@ -64,7 +80,16 @@ public class JavaGrepImp implements JavaGrep {
 
     @Override
     public List<String> readLines(File inputFile) {
-        return Collections.emptyList();
+        List<String> lines = new ArrayList<String>();
+        try (Scanner scanner = new Scanner(inputFile);) {
+            while(scanner.hasNext()) {
+                lines.add(scanner.nextLine());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("IO exception", e);
+        }
+//        logger.debug(String.join();
+        return lines;
     }
 
     @Override
@@ -74,7 +99,12 @@ public class JavaGrepImp implements JavaGrep {
 
     @Override
     public void writeToFile(List<String> lines) throws IOException {
-
+        String fileContent = String.join("\n", lines);
+        Path outPath = Paths.get(this.outFile);
+        if(!Files.exists(outPath)) {
+            Files.createFile(outPath);
+        }
+        Files.write(outPath, fileContent.getBytes());
     }
 
     @Override
