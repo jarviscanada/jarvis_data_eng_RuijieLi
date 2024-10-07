31f746481b48c97f1d9719c0f288c91b13eb6179
Rui Jie Li <rui-jie.li@polymtl.ca>
2024-10-07 18:45:37 -0400

    added some unit tests

--- core_java/jdbc/src/test/java/ca/jrvs/stockquote/service/QuoteService_UnitTest.java
+++ core_java/jdbc/src/test/java/ca/jrvs/stockquote/service/QuoteService_UnitTest.java
@@ -1,10 +1,14 @@
 package ca.jrvs.stockquote.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.text.ParseException;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -41,4 +45,34 @@ public class QuoteService_UnitTest {
             this.quoteService.fetch("SLSDJFLS");
         });
     }
+    @Test
+    public void testFetchFromDB_inDB() throws ParseException {
+        Quote quote = TestQuoteUtil.getDefaultQuote();
+        when(this.quoteDao.findById("DOGE")).thenReturn(Optional.of(TestQuoteUtil.getDefaultQuote()));
+        Quote returnedQuote = this.quoteService.fetchFromDB("DOGE").get();
+        assertTrue(quote.equals(returnedQuote));
+    }
+    @Test
+    public void testFetchFromDB_notInDB() throws ParseException {
+        Quote quote = TestQuoteUtil.getDefaultQuote();
+        quote.setTicker("QWERTY");
+        when(this.quoteDao.findById("QWERTY")).thenReturn(Optional.empty());
+        assertTrue(!this.quoteService.fetchFromDB("QWERTY").isPresent());
+    }
+    @Test
+    public void testFetch_inDB() throws ParseException, InvalidTickerException {
+        Quote quote = TestQuoteUtil.getDefaultQuote();
+        when(this.quoteDao.findById("DOGE")).thenReturn(Optional.of(TestQuoteUtil.getDefaultQuote()));
+        assertTrue(quote.equals(this.quoteService.fetch("DOGE").get()));
+    }
+    @Test
+    public void testFetch_notInDB_inAPI() throws ParseException, InvalidTickerException {
+        Quote quote = TestQuoteUtil.getDefaultQuote();
+        quote.setTicker("TSLA");
+        when(this.quoteDao.findById("TSLA")).thenReturn(Optional.empty());
+        when(this.quoteDao.save(any())).thenReturn(quote);
+        when(this.helper.fetchQuoteInfo("TSLA")).thenReturn(quote);
+        assertTrue(quote.equals(this.quoteService.fetch("TSLA").get()));
+    }
+    
 }
